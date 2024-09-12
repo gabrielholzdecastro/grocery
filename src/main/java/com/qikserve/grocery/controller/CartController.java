ccd61659be931a16929a9aed43ec4cc83a4078f5
gabriel.holz <gabrielholzdecastro@outlook.com>
2024-09-11 23:08:45 -0300

    feat: add getAll method to CartController

--- src/main/java/com/qikserve/grocery/controller/CartController.java
+++ src/main/java/com/qikserve/grocery/controller/CartController.java
@@ -5,13 +5,16 @@ import com.qikserve.grocery.service.CartService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/cart")
 public class CartController {
 
@@ -27,6 +30,11 @@ public class CartController {
     public ResponseEntity<Void> clear() {
         cartService.clear();
         return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<Cart> getALl() {
+        return ResponseEntity.ok(cartService.getCart());
     }
 
 }
